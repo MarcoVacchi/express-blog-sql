6ddabcb8eb9386437ea06701c23978202ff2b4e7
MarcoVacchi <mvacchi816@gmail.com>
2025-05-08 14:05:28 +0200

    feat: complete exercise

--- controllers/controllers.posts.js
+++ controllers/controllers.posts.js
@@ -18,21 +18,32 @@ function destroy(req, res) {
   posts.query('DELETE FROM posts WHERE id = ?', [id], (err) => {
     if (err) return res.status(500).json({ error: 'Failed to delete pizza' });
     res.sendStatus(204)
+  })
+};
+
+function show(req, res) {
+  // recuperiamo l'id dall' URL
+  const id = req.params.id
+  const sql = 'SELECT * FROM posts WHERE id = ?';
+  posts.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).json({ error: 'Database query failed' });
+    if (results.length === 0) return res.status(404).json({ error: 'Pizza not found' });
+    res.json(results[0]);
   });
 }
 
-function show(req, res) {
+// function show(req, res) {
 
-  const currentId = Number(req.params.id);
+//   const currentId = Number(req.params.id);
 
-  const currentPost = posts.find(posts => posts.id === currentId);
+//   const currentPost = posts.find(posts => posts.id === currentId);
 
-  currentPost ? res.json(currentPost)
-    : res.status(404).json({
-      error: "Id non trovato",
-      message: "Inserisci un id valido!"
-    });
-};
+//   currentPost ? res.json(currentPost)
+//     : res.status(404).json({
+//       error: "Id non trovato",
+//       message: "Inserisci un id valido!"
+//     });
+// };
 
 
 // function destroy(req, res) {
